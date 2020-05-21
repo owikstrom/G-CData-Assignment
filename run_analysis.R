af81564da9dac971877aea72e460e07b5408c07d
Olov Wikstrom <olov.wikstrom@gmail.com>
2020-05-21 16:42:00 +0200

    Implemented the tidying functions

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,41 @@
+library(dplyr)
 
-# Merges the training and the test sets to create one data set.
+# Download files
+url1 <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+download.file(url1, basename(url1))
+filenames <- unzip(basename(url1), list=TRUE)
+
+# Read all necessary tables intp memory
 x_test <- read.table("UCI HAR Dataset/test/x_test.txt")
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-features <- read.table("UCI HAR Dataset/features.txt")
-labels <= read.table("UCI HAR Dataset/activity_labels.txt")
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-
-x_join <- rbind(x_test, x_train)
-y_join <- rbind(y_test, y_train)
-subject_join <- rbind(subject_test, subject_train)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 
-# Extracts only the measurements on the mean and standard deviation for each measurement.
-colselect <- features[grep("(mean|std)\\(", features$V2),]
+# Bind the different train and test sets
+x_full <- rbind(x_test, x_train)
+y_full <- rbind(y_test, y_train)
+subject_full <- rbind(subject_test, subject_train)
+
+# Select mean & std columns and add proper column names to reduced set
+colnames(x_full) <- gsub("\\(|\\)","",features$V2) #Set titles without parenthesis
+cols <- grep("(mean|std)\\(",features$V2)
+x_reduced <- x_full[,cols]
 
 
-# Uses descriptive activity names to name the activities in the data set
-activity_names <- merge(y_test, y_train, labels)
 
-# Appropriately labels the data set with descriptive variable names.
-t1 <- x_test[, colselect$V1]
+# Consolidate the tidy dataset with subject and activity data
+y_full$activity <-factor(y_full$V1, labels = labels$V2)
+tidydataset <- cbind(activity= y_full$activity, subject=subject_full$V1, x_reduced)
 
 
-# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Cleanup
+list1 <- ls()
+list1 <- c(list1[list1 !='tidydataset'], 'list1')
+rm(list = list1)
+
+sumdataset <- tidydataset %>% group_by(subject, activity) %>% summarise_all(list(mean))
